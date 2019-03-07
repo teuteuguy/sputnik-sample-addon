b9576f1a2812a74f42f434fcfd3eebb0b46c800f
teuteuguy <timothee.cruse@gmail.com>
2019-03-07 15:18:31 +0800

    First commit

---
+++ src/app/sputnik-sample-addon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sputnik-sample-addon-component',
+    template: `
+        <h3>Hi, I am the Sample Addon component.</h3>
+    `
+})
+export class SputnikSampleAddonComponent {
+    constructor() {}
+}
